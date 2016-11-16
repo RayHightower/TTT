168b015e208fc7f2f350569e24b38c6289aac3df
Raymond T. Hightower <rth@wisdomgroup.com>
2016-11-16 17:06:32 -0600

    Begin building the grid.

--- TTT/Grid.cs
+++ TTT/Grid.cs
@@ -5,6 +5,20 @@ namespace TTT
 	{
 		public Grid()
 		{
+			char[] gridvalues = new char[10];
+
+			LoadGrid(gridvalues);
+
 		}
+
+		static char[] LoadGrid(char[] grid)
+		{
+			for (int i = 1; i < 10; i++)
+			{
+				grid[i] = (char)(i + 48);
+			}
+			return grid;
+		}
+
 	}
 }
